cb8e4cff63197ffdb2625889c02f7eb9e16c88f6
nbduykhang <nbduykhang@gmail.com>
2018-12-17 01:26:44 +0700

    sd

--- resources/views/client/diachigiaohang/edit.blade.php
+++ resources/views/client/diachigiaohang/edit.blade.php
@@ -41,7 +41,7 @@
                         	</select></li>
                         	<li><select name="cliedit_district" placeholder="Quận/Huyện"/>
                         		@for($a=1;$a<=10;$a++)
-									@if($address->quanhuyen == $a)
+									             @if($address->quanhuyen == $a)
                             	<option value="{{ $a }}" selected>Quận {{ $a }}</option>
                             		@else
                             	<option value="{{ $a }}">Quận {{ $a }}</option>
