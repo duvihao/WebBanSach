4f1d4f9bfb35e0273d7fca2c9b2a3dbedfa6a67b
nbduykhang <nbduykhang@gmail.com>
2018-12-17 01:33:55 +0700

    Create DonHang.php

---
+++ app/DonHang.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DonHang extends Model
+{
+    //
+    protected $fillable = [
+        'madh', 'makh', 'tongtien', 'hinhthucthanhtoan', 'xusudung', 'giamgia', 'phiship', 'diachigiaohang', 'trangthai'
+    ];
+
+    public function chitietdonhang() {
+        return $this->hasMany('App\ChiTietDonHang');
+    }
+
+    public function khachhang() {
+        return $this->belongsTo('App\KhachHang');
+    }
+
+    public function hinhthucthanhtoan() {
+        return $this->belongsTo('App\HinhThucThanhToan');
+    }
+
+    public function diachigiaohang() {
+        return $this->belongsTo('App\SoDiaChi');
+    }
+}
